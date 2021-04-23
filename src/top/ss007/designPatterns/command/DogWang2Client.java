8ccf2ae96a9271acecb1817ff255d1a46487fd4b
Ben.Wang <Ben.Wang@homecreditcfc.cn>
2021-04-23 09:24:14 +0800

    feature: add command pattern demo

--- src/top/ss007/designPatterns/command/DogWang2Client.java
+++ src/top/ss007/designPatterns/command/DogWang2Client.java
@@ -4,6 +4,7 @@ public class DogWang2Client {
 
     //享受xa机器人的服务
     public void enjoySexRobot() {
+        //robot 控制系统，用户通过此系统来发出命令
         RobotInvoker invoker = new RobotInvoker();
 
         //生成脱衣命令
@@ -11,17 +12,16 @@ public class DogWang2Client {
         TuoXiongZhaoCommand tuoXiongZhaoCommand = new TuoXiongZhaoCommand(bingBingReceiver);
         TuoKuZiCommand tuoKuZiCommand = new TuoKuZiCommand(bingBingReceiver);
         //构建执行计划
-        invoker.setTuoYiCommands(tuoXiongZhaoCommand, tuoKuZiCommand);
+        invoker.addCommands(tuoXiongZhaoCommand);
+        invoker.addCommands(tuoKuZiCommand);
 
         //生成跳舞命令
         YangMiReceiver yangMiReceiver = new YangMiReceiver();
         TiaoLaWuCommand tiaoLaWuCommand = new TiaoLaWuCommand(yangMiReceiver, "半小时");
         //构建执行计划
-        invoker.setLaWuCommand(tiaoLaWuCommand);
+        invoker.addCommands(tiaoLaWuCommand);
 
         //执行命令
-        invoker.enjoyTuoYi();
-        invoker.enjoyLaWu();
-
+        invoker.executeCommand();
     }
 }
--- src/top/ss007/designPatterns/command/RobotInvoker.java
+++ src/top/ss007/designPatterns/command/RobotInvoker.java
@@ -5,32 +5,22 @@ import java.util.List;
 
 //调用者必须认识各种具体的命令，因为它要按照要求组织并触发这些命令
 public class RobotInvoker {
-    private final List<Command> tuoYiCommands = new ArrayList<>();
-    private TiaoLaWuCommand laWuCommand;
+    private final List<Command> sexRobotCommands = new ArrayList<>();
 
-    //设置一套脱衣命令，不知道具体执行者是谁
-    public void setTuoYiCommands(TuoXiongZhaoCommand xzCommand, TuoKuZiCommand kzCommand) {
-        tuoYiCommands.clear();
-        tuoYiCommands.add(xzCommand);
-        tuoYiCommands.add(kzCommand);
+    public void clearCommand(){
+        sexRobotCommands.clear();
     }
 
-    //设置跳辣舞的命令
-    public void setLaWuCommand(TiaoLaWuCommand laWuCommand) {
-        this.laWuCommand = laWuCommand;
+    //设置一套命令，不知道具体执行者是谁
+    public void addCommands(Command command) {
+        sexRobotCommands.add(command);
     }
 
     //执行脱衣系列命令
-    public void enjoyTuoYi() {
-        for (Command tuoYiCommand : tuoYiCommands) {
+    public void executeCommand() {
+        for (Command tuoYiCommand : sexRobotCommands) {
             tuoYiCommand.execute();
         }
     }
 
-    //执行跳辣舞命令
-    public void enjoyLaWu(){
-        if(laWuCommand!=null){
-            laWuCommand.execute();
-        }
-    }
 }
